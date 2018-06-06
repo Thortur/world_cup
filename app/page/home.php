a46b089283e3d9aa6f55b206a41edf86a3a41e27
Thortur <Thortur@hotmail.fr>
2018-06-06 17:11:08 +0200

    retoure de la page home

---
+++ app/page/home.php
@@ -0,0 +1,11 @@
+<?php	
+declare (strict_types = 1);	
+namespace worldCup;	
+header('Content-Type: text/html; charset=UTF-8');	
+	
+require_once './../fonctions.php';	
+require_once './../src/header.php';	
+require_once './../src/nav.php';	
+for($i = 0; $i<=100;$i++) echo $i.'<br/>';	
+	
+require_once './../src/footer.php'; 
