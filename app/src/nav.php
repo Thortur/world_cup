c09c2f02836e30ad232fd1d6d696b6fbe43a538c
Thortur <Thortur@hotmail.fr>
2018-06-07 16:17:47 +0200

    work in navbar

--- app/src/nav.php
+++ app/src/nav.php
@@ -1,28 +1,51 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="/app/page/home.php" id="lienLogoNavBar"><img alt="wc-russia-2018.png" src="/src/images/wc-russia-2018.png" id="logoNavBar" /></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuPrincipale" aria-controls="menuPrincipale" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
-    <div class="collapse navbar-collapse" id="navbarColor02">
+    <div class="collapse navbar-collapse" id="menuPrincipale">
         <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="/app/page/home.php">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <!-- <li class="nav-item">
-            <a class="nav-link" href="./page/home.php">Features</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./page/home.php">Pricing</a>
-        </li> -->
-        <li class="nav-item">
-            <a class="nav-link" href="/app/page/about.php">?</a>
-        </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/app/page/home.php">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/app/page/bet.php">Pari</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/app/page/classement.php">Classement</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/app/page/groupe.php">Groupe</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Résultat
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/app/page/resultatMatch.php">Liste des matchs</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=1">Groupe A</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=2">Groupe B</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=3">Groupe C</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=4">Groupe D</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=5">Groupe E</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=6">Groupe F</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=7">Groupe G</a>
+                    <a class="dropdown-item" href="/app/page/resultatGroupe.php?idGroupe=8">Groupe H</a>
+
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/app/page/about.php">?</a>
+            </li>
         </ul>
-        <form class="form-inline">
+        <div class="form-inline">
+            <button type="submit" class="btn btn-primary" name="deconnexion" >Se déconnecter</button>
+        </div>
+        <!-- <form class="form-inline">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        </form> -->
     </div>
 </nav>
 <div style="height:56px;"></div>
