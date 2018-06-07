6d9e2b535a50375e0dd2fb32f183dfd315315c4c
Thortur <Thortur@hotmail.fr>
2018-06-07 16:20:21 +0200

    work in progress

--- app/src/header.php
+++ app/src/header.php
@@ -5,7 +5,7 @@
         <title>Title of the document</title>
         <link rel="stylesheet" href="/src/bootstrap-4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="/src/fontawesome-free-5.0.13/web-fonts-with-css/css/fontawesome-all.min.css">
-        <link rel="stylesheet" href="/app/src/nav.css">
+        <link rel="stylesheet" href="/app/src/css/nav.css">
         <script type="text/javascript" src="/src/jquery/jquery-3.3.1.min.js"></script>
     </head>
     <body>
