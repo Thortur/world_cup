fc0c0d6a0da7fa2d7d79f82321bfacedc9f5c70a
Thortur <Thortur@hotmail.fr>
2018-06-07 17:09:19 +0200

    zoom mobile

--- app/src/header.php
+++ app/src/header.php
@@ -2,7 +2,8 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Title of the document</title>
+        <title>Pari entre amis</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/src/bootstrap-4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="/src/fontawesome-free-5.0.13/web-fonts-with-css/css/fontawesome-all.min.css">
         <link rel="stylesheet" href="/app/src/css/nav.css">
