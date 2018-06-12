2530d62bc357db8c397b6f98c4c3e067cc233455
Thortur <Thortur@hotmail.fr>
2018-06-12 18:08:33 +0200

    modif dashboard

--- app/page/home/index.php
+++ app/page/home/index.php
@@ -31,6 +31,28 @@ $datas       = $SendRequete->exec();
 $TimeLine    = new TimeLine($datas);
 $tabMatch    = $TimeLine->getTabMatch();
 
+
+function getCardUser () {
+    $html = '
+    <div class="card profile-card-with-stats">
+        <div class="text-center">
+            <div class="card-body">
+                <img src="/app/src/images/portrait/small/avatar-s-21.png" class="rounded-circle  height-150" alt="Card image">
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">@'.$_SESSION['world_cup']['login']['pseudo'].'</h4>
+                <h3 class="card-title">@Cagnotte</h3>
+            </div>
+            <div class="card-body">
+                <button type="button" class="btn btn-outline-danger btn-md mr-1"><i class="fa fa-plus"></i> Passer Premium</button>
+                <button type="button" class="btn btn-outline-primary btn-md mr-1"><i class="ft-user"></i> Voir le profil</button>
+            </div>
+        </div>
+    </div>
+    ';
+
+    return $html;
+    }
 ?>
 <!DOCTYPE html>
 <html class="loading" lang="fr" data-textdirection="ltr">
@@ -40,15 +62,18 @@ $tabMatch    = $TimeLine->getTabMatch();
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
         <link rel="stylesheet" type="text/css" href="/src/bootstrap-4.0.0/css/bootstrap.min.css" id="bootstrap-css">
         <link rel="stylesheet" type="text/css" href="/src/fontawesome-free-5.0.13/web-fonts-with-css/css/fontawesome-all.min.css">
-        <link rel="stylesheet" type="text/css" href="/app/src/css/nav.css">
         <link rel="stylesheet" type="text/css" href="/app/src/css/flag-icon.min.css">
         <link rel="stylesheet" type="text/css" href="/app/src/css/feather.min.css">
         <link rel="stylesheet" type="text/css" href="./css/pace.css">
         <link rel="stylesheet" type="text/css" href="./css/timeline.css">
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap-extended.css">
+        <link rel="stylesheet" type="text/css" href="./css/colors.css">
+        <link rel="stylesheet" type="text/css" href="./css/components.css">
+        <link rel="stylesheet" type="text/css" href="/app/src/css/nav.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
         rel="stylesheet">
     </head>
-    <body class="horizontal-layout horizontal-menu 2-columns" data-open="hover" data-menu="horizontal-menu" data-col="2-columns">
+    <body class"2-columns">
         <?php require_once "./../../src/nav.php"; ?>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <div class="app-content content">
@@ -57,12 +82,12 @@ $tabMatch    = $TimeLine->getTabMatch();
                         <div class="row match-height">
                             <div class="col-xl-8 col-lg-12">
                             <?php
-                                echo $TimeLine->getCardParis($tabMatch);
+                            echo $TimeLine->getCardParis($tabMatch);
                             ?>
                             </div>
                             <div class="col-xl-4 col-lg-12">
                             <?php
-                            // echo getCardParis($tabMatch);
+                            echo getCardUser();
                             ?>
                             </div>
                         </div>
@@ -71,6 +96,8 @@ $tabMatch    = $TimeLine->getTabMatch();
             </div>
             <script src="./js/vendors.min.js" type="text/javascript"></script>
             <script src="./js/horizontal-timeline.js" type="text/javascript"></script>
+            <script src="./js/app-menu.js" type="text/javascript"></script>
+            <script src="./js/app.js" type="text/javascript"></script>
             <script src="./js/home.js" type="text/javascript"></script>
         </form>
     </body>
